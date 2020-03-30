5562149d729ef2c966cb47add48e17ef79b81e0c
Justin Proulx <jawproulx2001@icloud.com>
2020-03-30 18:55:52 -0400

    Update TinyHashes.swift

--- Sources/TinyHashes/TinyHashes.swift
+++ Sources/TinyHashes/TinyHashes.swift
@@ -1,5 +1,4 @@
 import CommonCrypto
-import CryptoKit
 import Foundation
 
 private func hexString(_ iterator: Array<UInt8>.Iterator) -> String {
@@ -9,27 +8,19 @@ private func hexString(_ iterator: Array<UInt8>.Iterator) -> String {
 extension Data {
 
     public var sha1: String {
-        if #available(iOS 13.0, macOS 10.15, *) {
-            return hexString(Insecure.SHA1.hash(data: self).makeIterator())
-        } else {
-            var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
-            self.withUnsafeBytes { bytes in
-                _ = CC_SHA1(bytes.baseAddress, CC_LONG(self.count), &digest)
-            }
-            return hexString(digest.makeIterator())
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+        self.withUnsafeBytes { bytes in
+            _ = CC_SHA1(bytes.baseAddress, CC_LONG(self.count), &digest)
         }
+        return hexString(digest.makeIterator())
     }
 
     public var sha256: String {
-        if #available(iOS 13.0, macOS 10.15, *) {
-            return hexString(SHA256.hash(data: self).makeIterator())
-        } else {
-            var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-            self.withUnsafeBytes { bytes in
-                _ = CC_SHA256(bytes.baseAddress, CC_LONG(self.count), &digest)
-            }
-            return hexString(digest.makeIterator())
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        self.withUnsafeBytes { bytes in
+            _ = CC_SHA256(bytes.baseAddress, CC_LONG(self.count), &digest)
         }
+        return hexString(digest.makeIterator())
     }
 
 }
